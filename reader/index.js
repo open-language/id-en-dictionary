671628af0a3ecc361989897e1979bff0d3623e47
Sudhanshu Raheja <sudhanshu@leftshift.io>
2019-06-03 23:14:37 +0700

    [sudhanshu] adds reader

---
+++ reader/index.js
@@ -0,0 +1,51 @@
+const wordnet = require('id-wordnet')
+const readline = require('readline')
+const fs = require('fs')
+const parser = require('../parser')
+const dictionary = require('../dictionary')
+
+const version = "1.2"
+const wordnetPath = wordnet[version]
+
+const fileTypes = ['wn-ind-def.tab', 'wn-msa-all.tab']
+
+const reader = {
+    isReady: false,
+    readRemaining: 2,
+
+    init: () => {
+        fileTypes.forEach((fileType) => {
+            reader.read(fileType)
+        })
+    },
+
+    read: (fileType) => {
+        const file = `${wordnetPath}/${fileType}`
+        const readerInterface = readline.createInterface({
+            input: fs.createReadStream(file),
+            output: false
+        })
+
+        readerInterface.on('line', (line) => {
+            if (fileType === 'wn-ind-def.tab') {
+                const item = new parser.DefinitionLine(line)
+                dictionary.addIndex(item)
+            } else {
+                const item = new parser.AllLine(line)
+                dictionary.addData(item)
+            }
+        })
+
+        readerInterface.on('close', () => {
+            reader.readRemaining -= 1
+            if (reader.readRemaining === 0) {
+                reader.isReady = true
+                dictionary.readComplete()
+            }
+        })
+
+        // Ignoring close, pause, resume, SIGCONT, SIGINT, SIGTSTP
+    },
+}
+
+module.exports = reader
