a323f435d51fd3a00e42aecf7136b0722735db28
Sudhanshu Raheja <sudhanshu@leftshift.io>
2019-06-05 16:23:50 +0700

    [sudhanshu] fixed test for package initialise

--- index.test.js
+++ index.test.js
@@ -1,7 +1,9 @@
-const idDictionary = require('./index')
+const p = require('./index')
 
 describe("Test the index file for IdDictionary", () => {
     test("Test initialization", async () => {
-        await idDictionary.init()
+        const dict = await p.init()
+        const result = dict.query('mengerti')
+        expect(result['589738'].words).toEqual('memahami, mengerti, faham')
     }, 20000)
 })
